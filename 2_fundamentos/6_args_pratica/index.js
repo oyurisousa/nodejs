44e0ab911f235a471e46cfc422a547db5a1d6679
yuri <yurisousaenfer@gmail.com>
2023-07-01 09:21:55 -0300

    pratica argumentos

--- 2_fundamentos/6_args_pratica/index.js
+++ 2_fundamentos/6_args_pratica/index.js
@@ -1,8 +1,7 @@
-const soma = require('./soma').soma
+const soma = require("./soma").soma
 const minimist = require("minimist")
 
 const args = minimist(process.argv.slice(2))
 let a = parseInt(args['a'])
-let b = parseInt(args['b'])
-
+let b = parseInt(args['b']) 
 console.log(soma(a,b))
