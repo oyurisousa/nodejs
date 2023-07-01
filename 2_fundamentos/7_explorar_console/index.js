66d3786e9f4fec1be7017fcde100d0427b977ee5
yuri <yurisousaenfer@gmail.com>
2023-07-01 09:27:17 -0300

    explorando o console

---
+++ 2_fundamentos/7_explorar_console/index.js
@@ -0,0 +1,18 @@
+//log - mostrar uma mensagem na tela
+console.log('olá mundo!')
+
+//count - conta a quantidade de vezes que a msg foi printada
+console.count("olá")
+console.count("olá")
+
+//clear - limpa tudo que está acima no terminal
+
+//variavel entre string
+nome = 'yuri'
+console.log("%s",nome)
+
+setTimeout(()=>{
+    console.clear()
+    console.log('fim')
+},2000)
+
