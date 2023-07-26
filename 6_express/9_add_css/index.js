97a6f6b433992e6afa3f65c340387649f08f037e
yuri <yurisousaenfer@gmail.com>
2023-07-26 19:40:18 -0300

    adicionando files statics

---
+++ 6_express/9_add_css/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path  = require("path")
+
+const basePath = path.join(__dirname,'/templates')
+const port = 3000
+
+const app = express()
+
+
+
+const users = require('./users')
+//ler o body
+app.use(express.urlencoded({ 
+    extended: true
+}))
+
+app.use(express.json())
+
+//arquivos staticos
+app.use(express.static('public'))
+
+
+app.use('/users', users)
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(`${basePath}/index.html`,()=>{})
+})
+
+app.listen(port,()=>{
+    console.log("rodando")
+})
+
