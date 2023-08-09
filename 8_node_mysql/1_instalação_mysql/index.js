adf894e144beb8a3a266b3f4104397848d37147f
yuri <yurisousaenfer@gmail.com>
2023-08-09 11:56:03 -0300

    instalação mysql e config

---
+++ 8_node_mysql/1_instalação_mysql/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import mysql from 'mysql'
+
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql'
+})
+
+conn.connect((err)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log("conectado!")
+    app.listen(3000);
+})
