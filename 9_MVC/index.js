cc03ad70ba3e039242c0dfde74e6ae52b6a4e984
yuri sousa <yurisousaenfer@gmail.com>
2024-01-03 17:36:21 -0300

    create model task

--- 9_MVC/index.js
+++ 9_MVC/index.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
 
+const app = express()
 const conn = require("./db/conn")
 
-const app = express()
+const Task = require("./models/Task")
 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
@@ -14,4 +15,13 @@ app.use(express.urlencoded({
 app.use(express.json())
 app.use(express.static('public'))
 
-app.listen(3000)
+
+conn
+    .sync()
+    .then(()=>{
+        app.listen(3000)
+        console.log("ok!")
+    })
+    .catch((err)=>{
+        console.error(err)
+})
