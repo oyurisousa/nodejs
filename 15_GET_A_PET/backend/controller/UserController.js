c5e8d8ff1bd5766c9f2762d0ad3d1c22b2d9969f
yuri sousa <yurisousaenfer@gmail.com>
2024-01-31 15:16:29 -0300

    feat: getByUserId/checkUser

--- 15_GET_A_PET/backend/controller/UserController.js
+++ 15_GET_A_PET/backend/controller/UserController.js
@@ -1,6 +1,9 @@
-const creatUserToken = require("../helpers/create-user-token")
+const jwt = require("jsonwebtoken")
+const createUserToken = require("../helpers/create-user-token")
+const getToken = require("../helpers/get-token")
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const { default: mongoose } = require("mongoose")
 
 module.exports = class UserController{
     static async register(req,res){
@@ -52,11 +55,77 @@ module.exports = class UserController{
         })
         try {
             const newUser = await user.save() 
-            await creatUserToken(newUser,req,res)
+            await createUserToken(newUser,req,res)
             
         } catch (error) {
             res.status(500).json({message: error})
         }
         
+    }
+
+    static async login(req,res){
+        const {email, password} = req.body
+
+        if(!email){
+            res.status(422).json({message: "the email field is required"})
+            return
+        }
+        if(!password){
+            res.status(422).json({message: "the password field is required"})
+            return
+        }
+
+        //check if user existss
+        const user = await User.findOne({email: email})
+        
+        if(!user){
+            res.status(422).json({message: "this user does not exists!"})
+            return
+        }
+
+
+        //check password match
+        
+        const checkMacthPassword = await bcrypt.compare(password, user.password)
+        if(!checkMacthPassword){
+            res.status(422).json({
+                message: `password incorrect!`
+            })
+            return
+        }
+
+        await createUserToken(user, req,res)
+    }  
+
+    static async checkUser(req,res){
+        let currentUser
+        console.log(req.headers.authorization)
+        if(req.headers.authorization){ 
+            const token = getToken(req)
+            const decoded = jwt.verify(token,'wesecret')
+            currentUser = await User.findById(decoded.id)
+
+            currentUser.password = undefined
+        }else{
+            currentUser = null
+        }
+        res.status(200).send(currentUser)
+    }
+
+    static async getUserById(req,res){
+        const {id} = req.params
+        let user
+        if(mongoose.Types.ObjectId.isValid(id)){ //validation id
+            user = await User.findById(id).select("-password")
+        }
+
+        if(!user){
+            res.status(422).json({
+                message: `user not found!`
+            })
+            return
+        }
+        
+        res.status(200).json({user})
     }
 }
--- 15_GET_A_PET/backend/routes/UserRoutes.js
+++ 15_GET_A_PET/backend/routes/UserRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router()
 
 const UserController = require("../controller/UserController")
 router.post('/register', UserController.register)
+router.post('/login', UserController.login)
+router.get('/checkuser', UserController.checkUser)
+router.get('/:id', UserController.getUserById)
 
 module.exports = router
