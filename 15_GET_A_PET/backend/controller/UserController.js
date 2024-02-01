dc594c796a3e4ed1080ea3a3f796c99578fc2dd3
yuri sousa <yurisousaenfer@gmail.com>
2024-02-01 15:14:08 -0300

    verify token

--- 15_GET_A_PET/backend/controller/UserController.js
+++ 15_GET_A_PET/backend/controller/UserController.js
@@ -128,4 +128,11 @@ module.exports = class UserController{
         
         res.status(200).json({user})
     }
+
+    static async editUser(req,res){
+        res.status(200).json({
+            message: "update sucessfully!"
+        })
+        return
+    }
 }
--- 15_GET_A_PET/backend/routes/UserRoutes.js
+++ 15_GET_A_PET/backend/routes/UserRoutes.js
@@ -2,9 +2,12 @@ const express = require("express")
 const router = express.Router()
 
 const UserController = require("../controller/UserController")
+const verifyToken = require("../helpers/verity-token")
+
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id',verifyToken, UserController.editUser)
 
 module.exports = router
