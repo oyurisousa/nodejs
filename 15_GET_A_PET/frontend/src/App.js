f665da38c1b2417411d046aea0e15939d59ae88f
yuri <yurisousaenfer@gmail.com>
2024-02-27 21:15:31 -0300

    start auth

--- 15_GET_A_PET/frontend/src/App.js
+++ 15_GET_A_PET/frontend/src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/pages/auth/Login';
+import Register from './components/pages/auth/Register';
+import Home from './components/pages/Home';
 function App() {
   return (
-    <div className="App">
-      <h1>Get A pet</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
