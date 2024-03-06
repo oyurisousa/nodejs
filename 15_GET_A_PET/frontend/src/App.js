7826ce3b399cf3371a7f642af08b3cc5d3089a37
yuri <yurisousaenfer@gmail.com>
2024-03-06 15:56:04 -0300

    ,module register

--- 15_GET_A_PET/frontend/src/App.js
+++ 15_GET_A_PET/frontend/src/App.js
@@ -1,15 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+/*components*/
+import Footer from './components/layouts/Footer';
+import Navbar from './components/layouts/Navbar';
+import Container from './components/layouts/Container';
+
 import Login from './components/pages/auth/Login';
 import Register from './components/pages/auth/Register';
 import Home from './components/pages/Home';
 function App() {
   return (
     <Router>
+      <Navbar/>
+      <Container>
       <Routes>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/' element={<Home/>}/>
       </Routes>
+      </Container>
+      <Footer/>
     </Router>
   );
 }
---
+++ 15_GET_A_PET/frontend/src/components/layouts/Navbar.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+import Logo from "../../assets/img/logo.png"
+import styles from "./Navbar.module.css"
+function Navbar(){
+    return (
+        <nav className={styles.navbar}>
+            <div>
+                <img src={Logo}></img>
+            </div>
+            <ul>
+                <li>
+                    <Link to={"/"}>Adopter</Link>
+                </li>
+                <li>
+                    <Link to={"/login"}>Login</Link>
+                </li>
+                <li>
+                    <Link to={"/register"}>Register</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
