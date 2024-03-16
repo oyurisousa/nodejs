00645c47bb7ac187a01eef75116e130ff3bd9923
yuri <yurisousaenfer@gmail.com>
2024-03-16 16:38:23 -0300

    dashboard my pets

--- 15_GET_A_PET/frontend/src/components/pages/Pet/Mypets.js
+++ 15_GET_A_PET/frontend/src/components/pages/Pet/Mypets.js
@@ -1,13 +1,68 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
+import api from "../../../utils/api";
+
+import RoundedImage from "../../layouts/RoundedImage";
+
+import styles from "./Dashboard.module.css";
+
+import useFlashMessage from "../../../hooks/useFlashMessage";
+
 function MyPets() {
 	const [pets, setPets] = useState([]);
+	const [token] = useState(localStorage.getItem("token") || "");
+	// const [setFlashMessage] = useFlashMessage();
+
+	useEffect(() => {
+		console.log("Fetching pets...");
+		api
+			.get("/pets/mypets", {
+				headers: {
+					Authorization: `Bearer ${JSON.parse(token)}`,
+				},
+			})
+			.then((response) => {
+				console.log("Pets data:", response.data.myPets);
+				setPets(response.data.myPets);
+			})
+			.catch((err) => {
+				console.error("Error fetching pets:", err);
+			});
+	}, [token]);
 	return (
 		<section>
-			<h1>MyPets</h1>
-			<Link to={"/pet/add"}>Cadastrar Pets</Link>
-			<div>
-				{pets.length > 0 && <p>Meus pets cadastrados</p>}
+			<div className={styles.petslist_header}>
+				<h1>Meus Pets Cadastrados</h1>
+				<Link to={"/pet/add"}>Cadastrar Pet</Link>
+			</div>
+			<div className={styles.petslist_container}>
+				{pets.length > 0 &&
+					pets.map((pet) => (
+						<div key={pet._id} className={styles.petlist_row}>
+							<RoundedImage
+								src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+								alt={pet.name}
+								width="px75"
+							/>
+							<span className="bold">{pet.name}</span>
+							<div className={styles.actions}>
+								{pet.available ? (
+									<>
+										{pet.adopter && (
+											<button className={styles.conclude_btn}>
+												Concluir adoção
+											</button>
+										)}
+										<Link to={`/pet/edit/${pet.id}`}>Editar</Link>
+										<button>Excluir</button>
+									</>
+								) : (
+									<p>Pet já adotado</p>
+								)}
+							</div>
+						</div>
+					))}
 				{pets.length === 0 && <p>Não há pets cadastrados</p>}
 			</div>
 		</section>
